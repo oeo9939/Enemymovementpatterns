45511838fb98f902b5e8cbf787e346fc3b6eded4
Teo <mgttmich@gmail.com>
2022-12-01 22:07:14 +0200

    Add enemy1, animate, fillRect, requestAnim, clearRect

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Enemymovementpatterns
+
+freeCodeCamp.org
+Project 3: Enemy movement patterns
+https://www.youtube.com/watch?v=GFO_txvwK_c&ab_channel=freeCodeCamp.org
+
+https://bevouliin.com/
--- main.js
+++ main.js
@@ -5,3 +5,23 @@ const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
+// enemy1 = {
+//     x: 10,
+//     y: 50,
+//     width: 100,
+//     height: 100,
+// }
+
+
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // endsless animation loop
+    enemy1.x++;
+    enemy1.y++;
+
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    requestAnimationFrame(animate);
+}
+
+animate();
