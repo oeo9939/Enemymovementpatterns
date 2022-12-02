310f40f2eb582a38f495cffb50e8834d09ac9950
Teo <mgttmich@gmail.com>
2022-12-02 18:28:01 +0200

    Set enemyImage constructor, this.frame-flapspeed-width-height-update()

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const enemiesArray = [];
 
 const enemyImage = new Image();
 enemyImage.src = "/assets/enemy1.png";
+let gameFrame = 0;
 
 // enemy1 = {
 //     x: 10,
@@ -24,16 +25,22 @@ class Enemy {
         this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
-        this.width = this.spriteWidth / 3;
-        this.height = this.spriteHeight / 3;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.random() * 3 + 1;
     }
     update() {
         this.x += this.speed;
         this.y += this.speed;
+        // animate sprites of enemyImage
+        if (gameFrame % 2 === 0 || this.flapSpeed < 2) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
     }
     draw() {
         ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
 
@@ -59,6 +66,7 @@ function animate() {
         enemy.draw();
     });
 
+    gameFrame++;
     // ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
     // ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
 
