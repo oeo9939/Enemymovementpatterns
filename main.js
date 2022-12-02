c69c0252c01cdfd6fcdc7faeea12b818edcfc310
Teo <mgttmich@gmail.com>
2022-12-02 17:47:13 +0200

    Set 100 enemies, numberofEn, enemiesArr, set const, forEach

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 // enemy1 = {
 //     x: 10,
@@ -14,33 +16,42 @@ CANVAS_HEIGHT = canvas.height = 1000;
 
 class Enemy {
     constructor() {
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.width = 100;
         this.height = 100;
+        this.speed = Math.random() * 4 - 2;
     }
     update() {
-        this.x++;
-        this.y++;
+        this.x += this.speed;
+        this.y += this.speed;
     }
     draw() {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 };
 
-const enemy1 = new Enemy();
+// const enemy1 = new Enemy();
 // const enemy2 = new Enemy();
 
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+};
+
+console.log(enemiesArray);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // endsless animation loop
-    enemy1.update();
-    enemy1.draw();
+    // enemy1.update();
+    // enemy1.draw();
     // enemy1.y++;
     // enemy2.x+=0.5;
     // enemy2.y+=0.5;
-
+    enemiesArray.forEach((enemy) => {
+        enemy.update();
+        enemy.draw();
+    });
 
     // ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
     // ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
