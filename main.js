cca87966986bdba3f4afc01a1afef509218338fc
Teo <mgttmich@gmail.com>
2022-12-02 18:05:36 +0200

    Add enemy1.png, enemyImg, spriteWidth-Height, drawImage

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+const enemyImage = new Image();
+enemyImage.src = "/assets/enemy1.png";
+
 // enemy1 = {
 //     x: 10,
 //     y: 50,
@@ -18,16 +21,19 @@ class Enemy {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
         this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 3;
+        this.height = this.spriteHeight / 3;
     }
     update() {
         this.x += this.speed;
         this.y += this.speed;
     }
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
 
@@ -38,7 +44,7 @@ for (let i = 0; i < numberOfEnemies; i++) {
     enemiesArray.push(new Enemy());
 };
 
-console.log(enemiesArray);
+// console.log(enemiesArray);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
