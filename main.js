e49ccf6d0e8cc776c563c60332cdde30800f29cb
Teo <mgttmich@gmail.com>
2022-12-03 18:04:15 +0200

    Add flapspeed, this.x.y, this.image, enemy1 complete

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = "/assets/enemy1.png";
+// const enemyImage = new Image();
+// enemyImage.src = "/assets/enemy1.png";
 let gameFrame = 0;
 
 // enemy1 = {
@@ -20,27 +20,29 @@ let gameFrame = 0;
 
 class Enemy {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        this.image = new Image();
+        this.image.src = "/assets/enemy1.png";
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
-        this.flapSpeed = Math.random() * 3 + 1;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;    //this.speed;
+        this.y += Math.random() * 5 - 2.5;    //this.speed;
         // animate sprites of enemyImage
-        if (gameFrame % 2 === 0 || this.flapSpeed < 2) {
+        if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        // ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
 
