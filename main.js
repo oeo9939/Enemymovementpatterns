a607549c741adb06bda3f9f8e007011ec09b4c11
Teo <mgttmich@gmail.com>
2022-12-03 18:33:04 +0200

    Set enemy2, this.angle, Math.sin, angleSpeed, curve, enemy2 complete

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 50;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 // const enemyImage = new Image();
@@ -31,10 +31,14 @@ class Enemy {
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
     update() {
         this.x -= this.speed;       //Math.random() * 5 - 2.5;
-        // this.y += Math.random() * 5 - 2.5;    //this.speed;
+        this.y += this.curve * Math.sin(this.angle);          //Math.random() * 5 - 2.5;    //this.speed;
+        this.angle += this.angleSpeed;
         if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites of enemyImage
         if (gameFrame % this.flapSpeed === 0) {
