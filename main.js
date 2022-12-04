5d1cdd6e191d0df7cc8437b2dca0384b8756b312
Teo <mgttmich@gmail.com>
2022-12-04 23:20:47 +0200

    enemy3 complete

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20;
+const numberOfEnemies = 50;
 const enemiesArray = [];
 
 // const enemyImage = new Image();
@@ -31,9 +31,9 @@ class Enemy {
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
+        this.angle = Math.random() * 500;
         this.angleSpeed = Math.random() * 2 + 0.5;
-        this.curve = Math.random() * 200 + 50;
+        // this.curve = Math.random() * 200 + 50;
     }
     update() {
         this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 90) + (canvas.width / 2 - this.width / 2);    // this.x -= this.speed;       //Math.random() * 5 - 2.5;
