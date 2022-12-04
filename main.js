7753b407caf3e35d5e8fd0ef094193a09f537938
Teo <mgttmich@gmail.com>
2022-12-04 23:33:55 +0200

    Add enemy4, newX, newY, update(), if, dx, dy, enemy4 complete

--- main.js
+++ main.js
@@ -21,25 +21,36 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = "/assets/enemy3.png";
+        this.image.src = "/assets/enemy4.png";
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 212;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * canvas.width;
+        this.newY = Math.random() * canvas.height;
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = Math.random() * 500;
-        this.angleSpeed = Math.random() * 2 + 0.5;
-        // this.curve = Math.random() * 200 + 50;
+        this.interval = Math.floor(Math.random() * 200 + 50);
+        // this.angle = Math.random() * 500;
+        // this.angleSpeed = Math.random() * 2 + 0.5;
+        // // this.curve = Math.random() * 200 + 50;
     }
     update() {
-        this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 90) + (canvas.width / 2 - this.width / 2);    // this.x -= this.speed;       //Math.random() * 5 - 2.5;
-        this.y = canvas.height / 2 * Math.sin(this.angle * Math.PI / 270) + (canvas.height / 2 - this.height / 2);    // this.x -= this.speed;       //Math.random() * 5 - 2.5;
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx / 20;
+        this.y -= dy / 20;
+        //this.x = 0;   //canvas.width / 2 * Math.cos(this.angle * Math.PI / 90) + (canvas.width / 2 - this.width / 2);    // this.x -= this.speed;       //Math.random() * 5 - 2.5;
+        //this.y = 0;   //canvas.height / 2 * Math.sin(this.angle * Math.PI / 270) + (canvas.height / 2 - this.height / 2);    // this.x -= this.speed;       //Math.random() * 5 - 2.5;
         // this.y += this.curve * Math.sin(this.angle);          //Math.random() * 5 - 2.5;    //this.speed;
-        this.angle += this.angleSpeed;
+        // this.angle += this.angleSpeed;
         if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites of enemyImage
         if (gameFrame % this.flapSpeed === 0) {
